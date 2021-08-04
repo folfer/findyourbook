d126c849d949b61b6b993c30d1f0ed5a8abc8694
folfer <victor.folfer@gmail.com>
2021-08-04 01:59:18 -0300

    Feat: Creating routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
+import Favorites from "../pages/Favorites";
+import Details from "../pages/Details";
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/favorites" component={Favorites} />
+      <Route path="/details/:id" component={Details} />
     </Switch>
   );
 };
