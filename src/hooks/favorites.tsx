c99910b0d85c76b5bd0d1cca9805792ac6c0456d
folfer <victor.folfer@gmail.com>
2021-08-04 01:54:44 -0300

    Feat: Creating context and setting favorite to localstorage

---
+++ src/hooks/favorites.tsx
@@ -0,0 +1,104 @@
+import React, {
+  useState,
+  useCallback,
+  useContext,
+  createContext,
+  useEffect,
+} from 'react';
+
+interface Book {
+  id: string;
+  selfLink: string;
+  volumeInfo: {
+    title: string;
+    subtitle?: string;
+    imageLinks: {
+      thumbnail: string;
+      smallThumbnail: string;
+    };
+    language?: string;
+    previewLink?: string;
+    publishedDate: string;
+  };
+}
+
+interface FavoriteContext {
+  favorites: Book[];
+  addFavorite(fav: Book): void;
+  removeFavorite(fav: Book): void;
+  clearFavorites(): void;
+}
+
+const FavoriteContext = createContext<FavoriteContext | null>(null);
+
+const FavoriteProvider: React.FC = ({ children }) => {
+  const [favorites, setFavorites] = useState<Book[]>([]);
+
+  // verifica se tem livros salvos e seta a variavel favorites
+  useEffect(() => {
+    function loadFavorites(): void {
+      const storagedFavorites = localStorage.getItem('@yourbook:favorites');
+      if (storagedFavorites) {
+        setFavorites([...JSON.parse(storagedFavorites)]);
+      }
+    }
+    loadFavorites();
+  }, []);
+
+  const removeFavorite = useCallback(
+    (fav: Book) => {
+      const favoritsWithoutRemoved = favorites.filter(f => f.id !== fav.id);
+      setFavorites(favoritsWithoutRemoved);
+      localStorage.setItem(
+        '@yourbook:favorites',
+        JSON.stringify(favoritsWithoutRemoved),
+      );
+    },
+    [favorites],
+  );
+
+  const addFavorite = useCallback(
+    (fav: Book) => {
+      const alreadyFavorite = favorites.find(f => f.id === fav.id);
+
+      if (alreadyFavorite) {
+        removeFavorite(fav);
+      } else {
+        localStorage.setItem(
+          '@yourbook:favorites',
+          JSON.stringify([...favorites, { ...fav }]),
+        );
+        setFavorites([...favorites, { ...fav }]);
+      }
+    },
+    [favorites, removeFavorite],
+  );
+
+  const clearFavorites = useCallback(() => {
+    setFavorites([]);
+    localStorage.setItem('@yourbook:favorites', '');
+  }, []);
+
+  const value = React.useMemo(
+    () => ({ favorites, addFavorite, removeFavorite, clearFavorites }),
+    [favorites, addFavorite, removeFavorite, clearFavorites],
+  );
+
+  return (
+    <FavoriteContext.Provider value={value}>
+      {children}
+    </FavoriteContext.Provider>
+  );
+};
+
+function useFavorite(): FavoriteContext {
+  const context = useContext(FavoriteContext);
+
+  if (!context) {
+    throw new Error('useFavorite must be used within FavoriteProvider');
+  }
+
+  return context;
+}
+
+export { FavoriteProvider, useFavorite };
