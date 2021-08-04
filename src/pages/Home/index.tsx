91a08058a69d5a88c1404374932d8e888ab1efa0
folfer <victor.folfer@gmail.com>
2021-08-04 01:57:25 -0300

    Feat: Creating homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,64 @@
 import React from "react";
-import { Container } from "./styles";
+import Card from "../../components/Card";
+import Search from "../../components/Search";
+import { useSearch } from "../../hooks/search";
+import { useFavorite } from "../../hooks/favorites";
+
+import {
+  Container,
+  Empty,
+  BookImage,
+  NotFound,
+  InformationText,
+  SearchImg,
+} from "./styles";
+import Pagination from "../../components/Pagination";
+import book from "../../assets/book.svg";
+import searchImg from "../../assets/search.svg";
 
 const Home: React.FC = () => {
-  return <Container />;
+  const { books } = useSearch();
+  const { favorites } = useFavorite();
+
+  return (
+    <>
+      <Search />
+      {books && Object.keys(books)?.length === 0 && (
+        <Empty>
+          <BookImage src={book} alt="Busca" />
+          <InformationText>
+            Faça uma busca por autor, livro ou palavra-chave
+          </InformationText>
+        </Empty>
+      )}
+
+      <Container>
+        {books &&
+          books.map((book) => (
+            <Card
+              key={book.id}
+              id={book.id}
+              volumeInfo={book.volumeInfo}
+              selfLink={book.selfLink}
+              favorite={!!favorites.find((f) => f.id === book.id)}
+            />
+          ))}
+      </Container>
+
+      {!books && (
+        <NotFound>
+          <InformationText>Não foram encontrados registros</InformationText>
+          <SearchImg src={searchImg} alt="Busca" />
+        </NotFound>
+      )}
+
+      {books && Object.keys(books)?.length > 0 && (
+        <>
+          <Pagination />
+        </>
+      )}
+    </>
+  );
 };
 
 export default Home;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,53 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 42px 24px;
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 16px;
+
+  background-color: #e6f2e3;
+
+  @media (min-width: 980px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+
+export const NotFound = styled.div`
   width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const SearchImg = styled.img`
+  width: 20%;
+`;
+
+export const InformationText = styled.strong`
+  font-size: 1.525rem;
+  margin-bottom: 1rem;
+
+  text-align: center;
+`;
+
+export const Empty = styled.div`
+  height: calc(100vh - 18vh);
+  max-width: 800px;
+  margin: 20px auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const BookImage = styled.img`
+  width: 70%;
+`;
+
+export const Text = styled.strong`
+  font-size: 13px;
 `;
